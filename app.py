ac7c3765d736099f04f1e3ee12589af861ef21f1
A-nieto96 <adrianfnieto1996@gmail.com>
2026-01-14 02:27:08 -0500

    primer deploy

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, send_file
 import qrcode
 from io import BytesIO
+import os
 
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="/static")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -41,4 +43,6 @@ def generador_qr():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
